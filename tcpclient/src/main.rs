bd96ff074aafb8f09550082b59b47e41c3c4c3f7
thanhhnguyen23 <thanh.h.nguyen23@gmail.com>
2022-03-26 22:19:45 -0400

    add server reply (raw bytes -> utf8) and print to terminal

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,8 +1,19 @@
-use std::net::TcpStream;
+use std::{net::TcpStream, io::{Write, Read}};
 
 fn main() {
-    let _stream = 
+    let mut stream = 
         TcpStream::connect("localhost:3000").unwrap();
+
+        stream.write("Hello".as_bytes()).unwrap();
+
+        let mut buffer = [0; 5];
+
+        stream.read(&mut buffer).unwrap();
+
+        println!(
+            "got response from server: {:?}",
+            std::str::from_utf8(&buffer).unwrap()
+        );
     
     // * NOTE: run on cli
     // scenario1> $ cargo run -p tcpclient
